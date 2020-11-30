c41c0b95b838b38be8ad32447d1aa34bc9a38fc4
Ayman <aymn.abdellatief@gmail.com>
2020-11-30 16:50:22 +0200

    Init App

---
+++ src/product.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Product extends Component {
+    state = {  }
+    render() { 
+        return ( <span>product1</span> );
+    }
+}
+ 
+export default Product;
